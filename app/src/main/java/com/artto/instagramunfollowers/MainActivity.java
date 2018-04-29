eba5acc61ee7f72906f4313791f3d929f6974908
Alexander Gordienko <8artto8@gmail.com>
2018-04-29 09:20:36 +0300

    double click back to exit

--- app/src/main/java/com/artto/instagramunfollowers/MainActivity.java
+++ app/src/main/java/com/artto/instagramunfollowers/MainActivity.java
@@ -2,14 +2,13 @@ package com.artto.instagramunfollowers;
 
 import android.annotation.SuppressLint;
 import android.app.AlertDialog;
-import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
@@ -18,7 +17,6 @@ import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.IOException;
@@ -68,6 +66,7 @@ public class MainActivity extends AppCompatActivity {
             startLoginActivity();
     }
 
+    @SuppressLint("CommitPrefEdits")
     private void initialize() {
         settings = getSharedPreferences("INSTAGRAM_UNFOLLOWERS", Context.MODE_PRIVATE);
         editor = settings.edit();
@@ -350,5 +349,24 @@ public class MainActivity extends AppCompatActivity {
             outState.putString("password", instagram.getPassword());
         }
         super.onSaveInstanceState(outState);
+    }
+
+    boolean doubleBackToExitPressedOnce = false;
+    @Override
+    public void onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed();
+            return;
+        }
+
+        this.doubleBackToExitPressedOnce = true;
+        Toast.makeText(this, R.string.backPressed, Toast.LENGTH_SHORT).show();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                doubleBackToExitPressedOnce=false;
+            }
+        }, 2000);
     }
 }
